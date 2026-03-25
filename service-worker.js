eff9fe05b9993064ce5bc4325edb7f7817869f2e
ntnhanauto-eng <ntnhan.auto@gmail.com>
2026-03-25 10:55:58 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hvs-" + "v10"; // 🔥 đổi version mỗi lần update
+const CACHE_NAME = "hvs-" + "v11"; // 🔥 đổi version mỗi lần update
 const urlsToCache = [
   "./",
   "./index.html",
